a42da863ef47b297afb90d22c489e0410a11b79d
Saravana-05 <saravana.kumar@skylimitdigital.com>
2026-02-17 16:11:41 +0530

    test deploy

--- citizen/app/routes/user_routes.py
+++ citizen/app/routes/user_routes.py
@@ -28,3 +28,13 @@ async def login(data: LoginSchema, session: AsyncSession = Depends(get_session))
         raise HTTPException(401, "Invalid credentials")
 
     return result
+
+@router.post("/login123")
+async def login(data: LoginSchema, session: AsyncSession = Depends(get_session)):
+
+    result = await login_user(data, session)
+
+    if not result:
+        raise HTTPException(401, "Invalid credentials")
+
+    return result
