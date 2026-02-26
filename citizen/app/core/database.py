62a1565387cccebf8e78f78e28bd8a2f6a2d75ec
Saravana-05 <144824987+Saravana-05@users.noreply.github.com>
2026-02-26 23:22:03 +0530

    Modify DATABASE_URL for live environment setup
    Updated DATABASE_URL for live environment and commented out local environment URL.

--- citizen/app/core/database.py
+++ citizen/app/core/database.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 # Make sure DATABASE_URL is correct (remove extra quote)
 # DATABASE_URL = "postgresql+asyncpg://postgres:12345@localhost:5432/citizenprints"
-DATABASE_URL = "mysql+asyncmy://root:Password%40123@localhost:3306/citizenprints"
+# DATABASE_URL = "mysql+asyncmy://root:Password%40123@localhost:3306/citizenprints" // local env
+# Live env
+DATABASE_URL = "mysql+asyncmy://citizen_user:StrongPassword123@localhost:3306/citizenprints" 
 
 
 # 1. Async engine
